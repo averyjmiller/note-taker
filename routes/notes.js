f32aa10d7196cb90725ee84f3f731af6fc04253e
Avery Miller <averymiller2060@gmail.com>
2023-07-17 14:13:50 -0400

    implemented notes POST request to read and append to the db.json file

--- routes/notes.js
+++ routes/notes.js
@@ -4,10 +4,37 @@ const uuid = require('../helpers/uuid');
 
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
+// GET Route for retrieving all the notes
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received for feedback`);
+  console.info(`${req.method} request received for notes`);
 
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for submitting a note
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to submit a note`);
+
+  const { title, text } = req.body;
+
+  if(title && text) {
+    const newNote = {
+      title,
+      text,
+      note_id: uuid(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    res.json(response);
+  } else {
+    res.json('Error in posting note');
+  }
 });
 
 module.exports = notes;
