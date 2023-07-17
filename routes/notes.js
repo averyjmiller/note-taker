c3731ed498176f52e33d171f80b367fa9066193b
Avery Miller <averymiller2060@gmail.com>
2023-07-17 13:39:14 -0400

    implemented notes GET request that reads from the db.json file

--- routes/notes.js
+++ routes/notes.js
@@ -1,3 +1,13 @@
 const notes = require('express').Router();
 
+const uuid = require('../helpers/uuid');
+
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for feedback`);
+
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
 module.exports = notes;
